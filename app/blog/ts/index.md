755482418a79a5fb21adee1f44514d491ae28831
chengcq <chengcq@getui.com>
2022-01-13 15:30:49 +0800

    feat(ts): type和interface区别

---
+++ app/blog/ts/index.md
@@ -0,0 +1,17 @@
+---
+title: 记录ts知识点
+date: 2021-10-11
+sidebar: 'auto'
+categories:
+  - TypeScript
+tags:
+  - TypeScript
+---
+
+## type 和 interface 的区别
+
+- type 可以为任何类型引入名称。例如基本类型，联合类型等
+- type 不支持继承
+- type 不会创建一个真正的名字
+- type 无法被实现(implements)，而接口可以被派生类实现
+- type 重名时编译器会抛出错误，接口重名时会产生合并
--- example/手写promise/promise.js
+++ example/手写promise/promise.js
@@ -1,182 +1,18 @@
-// const PENDING = 'pending';
-// const FULFILLED = 'fulfilled';
-// const REJECTED = 'rejected';
+const PENDING = 'pending';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
 
 // 解析promise，决定最终的promise的出口时resolve还是reject
-// function resolvePromise(promise, x, resolve, reject) {
-//   // 避免无限循环调用，自己等待自己的情况
-//   if (promise === x) {
-//     return reject(
-//       new TypeError('Chaining cycle detected for promise #<Promise>')
-//     );
-//   }
-
-//   // 锁，防止多次调用
-//   let called;
-
-//   // x是对象或者函数
-//   if ((x !== null && typeof x === 'object') || typeof x === 'function') {
-//     try {
-//       let then = x.then;
-//       if (typeof then === 'function') {
-//         then.call(
-//           x,
-//           y => {
-//             if (called) {
-//               return;
-//             }
-//             called = true;
-//             resolvePromise(promise, y, resolve, reject);
-//           },
-//           err => {
-//             // 成功和失败只能调用一个
-//             if (called) return;
-//             called = true;
-//             reject(err); // 失败了就失败了
-//           }
-//         );
-//       } else {
-//         resolve(x); // 直接成功即可
-//       }
-//     } catch (e) {
-//       // 走到 catch 也属于失败
-//       if (called) return;
-//       called = true;
-//       // 取then出错了那就不要在继续执行了
-//       reject(e);
-//     }
-//   } else {
-//     // 不是对象或者函数直接resolve
-//     resolve(x);
-//   }
-// }
-
-// class MyPromise {
-//   constructor(executor) {
-//     this.status = PENDING;
-//     this.value;
-//     this.reason;
-//     // 存放then的回调函数数组
-//     this.thenArr = [];
-//     // 存放catch的回调函数数组
-//     this.catchArr = [];
-//     const resolve = (res) => {
-//       if (this.status === PENDING) {
-//         this.value = res;
-//         this.status = FULFILLED;
-//         this.thenArr.forEach((fn) => fn());
-//       }
-//     };
-//     const reject = (err) => {
-//       if (this.status === PENDING) {
-//         this.reason = err;
-//         this.status = REJECTED;
-//         this.catchArr.forEach((fn) => fn());
-//       }
-//     };
-//     executor(resolve, reject);
-//   }
-
-//   // all方法
-//   static all(promises) {
-//     const result = [];
-//     let count = 0;
-//     return new MyPromise((resolve) => {
-//       const processResultByKey = (value) => {
-//         count++;
-//         result.push(value);
-//       };
-//       for (let i = 0, len = promises.length; i < len; i++) {
-//         // 不是promise实例的话，直接返回
-//         if (promises[i] instanceof MyPromise) {
-//           promises[i].then((res) => {
-//             processResultByKey(res);
-//           }, reject);
-//         } else {
-//           processResultByKey(promises[i]);
-//         }
-//       }
-//       if (count === result.length) {
-//         resolve(result);
-//       }
-//     });
-//   }
-
-//   // then需要返回一个promise
-//   then(onFulfilled, onRejected) {
-//     // 判断参数是否为函数，如果不是就直接给一个返回value的函数
-//     onFulfilled =
-//       typeof onFulfilled === 'function' ? onFulfilled : (value) => value;
-//     onRejected =
-//       typeof onRejected === 'function'
-//         ? onRejected
-//         : (err) => {
-//             throw err;
-//           };
-//     let promise;
-//     promise = new MyPromise((resolve, reject) => {
-//       // executor的状态为fulfilled
-//       if (this.status === FULFILLED) {
-//         setTimeout(() => {
-//           resolvePromise(promise, onFulfilled(this.value), resolve, reject);
-//         }, 0);
-//       }
-
-//       // executor的状态为rejected
-//       if (this.status === REJECTED) {
-//         setTimeout(() => {
-//           resolvePromise(promise, onRejected(this.err), resolve, reject);
-//         }, 0);
-//       }
-
-//       // 此处用来解决异步问题，如果依然是pending的话，可以先将then里面的回调函数放入队列，等待resolve或者reject再统一执行
-//       if (this.status === PENDING) {
-//         this.thenArr.push(() => {
-//           setTimeout(() => {
-//             resolvePromise(promise, onFulfilled(this.value), resolve, reject);
-//           }, 0);
-//         });
-//         this.catchArr.push(() => {
-//           setTimeout(() => {
-//             resolvePromise(promise, onRejected(this.err), resolve, reject);
-//           }, 0);
-//         });
-//       }
-//     });
-//     return promise;
-//   }
-
-//   catch(onRejected) {
-//     return this.then(null, onRejected);
-//   }
-// }
-
-// debugger;
-// const promiseA = new MyPromise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve(1);
-//   }, 1000);
-// });
-
-// promiseA.then(res => {
-//   debugger;
-//   document.write(res);
-// }, err => {
-//   console.log(err);
-// });
-
-// 链式调用的核心，
-// function resolvePromise(promise, x, resolve, reject) {}
-
 function resolvePromise(promise, x, resolve, reject) {
-  // 防止多次调用
-  if (x === promise) {
-    return reject(new TypeError('chaining cycle detected for promise'));
+  // 避免无限循环调用，自己等待自己的情况
+  if (promise === x) {
+    return reject(new TypeError('Chaining cycle detected for promise #<Promise>'));
   }
 
-  // 加锁，防止多次调用
+  // 锁，防止多次调用
   let called;
-  // x为对象或者函数
+
+  // x是对象或者函数
   if ((x !== null && typeof x === 'object') || typeof x === 'function') {
     try {
       let then = x.then;
@@ -184,86 +20,85 @@ function resolvePromise(promise, x, resolve, reject) {
         then.call(
           x,
           y => {
-            if (!called) {
-              called = true;
-              // 如果resolve之后还是promise就继续递归来解析
-              resolvePromise(promise, y, resolve, reject);
+            if (called) {
+              return;
             }
+            called = true;
+            resolvePromise(promise, y, resolve, reject);
           },
           err => {
-            if (!called) {
-              called = true;
-              // 捕获到错误了就直接reject
-              reject(err);
-            }
+            // 成功和失败只能调用一个
+            if (called) return;
+            called = true;
+            reject(err); // 失败了就失败了
           }
         );
       } else {
-        resolve(x);
+        resolve(x); // 直接成功即可
       }
-    } catch (error) {
-      if (!called) {
-        called = true;
-        reject(error);
-      }
+    } catch (e) {
+      // 走到 catch 也属于失败
+      if (called) return;
+      called = true;
+      // 取then出错了那就不要在继续执行了
+      reject(e);
     }
   } else {
+    // 不是对象或者函数直接resolve
     resolve(x);
   }
 }
 
-const PENDING = 'pending';
-const FULFILLED = 'fulfilled';
-const REJECTED = 'rejected';
 class MyPromise {
-  status;
-  reason;
-  value;
-  flag;
-  thenArr = [];
-  catchArr = [];
-  constructor(executor, flag) {
+  constructor(executor) {
     this.status = PENDING;
-    this.flag = flag;
-    // 成功回调
+    this.value;
+    this.reason;
+    // 存放then的回调函数数组
+    this.thenArr = [];
+    // 存放catch的回调函数数组
+    this.catchArr = [];
     const resolve = res => {
       if (this.status === PENDING) {
-        this.status = FULFILLED;
         this.value = res;
-        // 如果是pending的状态
+        this.status = FULFILLED;
         this.thenArr.forEach(fn => fn());
       }
     };
-
-    // 失败回调
     const reject = err => {
       if (this.status === PENDING) {
-        this.status = REJECTED;
         this.reason = err;
-        // 如果是同步函数的话，包装成异步
+        this.status = REJECTED;
         this.catchArr.forEach(fn => fn());
       }
     };
     executor(resolve, reject);
   }
 
-  // 静态的resolve函数
-  static resolve(value) {
-    if (value instanceof MyPromise) {
-      return value.then(resolve, reject);
-    }
-    if (this.status === PENDING) {
-      this.status = FULFILLED;
-      this.value = value;
-      this.thenArr.forEach(fn => fn());
-    }
-    return new MyPromise(resolve => {
-      resolve(value);
+  // all方法
+  static all(promises) {
+    if (!Array.isArray(promises)) throw new Error('input is not a array');
+
+    return new Promise((resovle, reject) => {
+      let count = 0;
+      const result = [];
+
+      for (let i = 0; i < promises.length; i++) {
+        promises[i]
+          .then(res => {
+            result[i] = res;
+            count++;
+
+            if (count === promises.length) resovle(result);
+          })
+          .catch(reject);
+      }
     });
   }
 
+  // then需要返回一个promise
   then(onFulfilled, onRejected) {
-    // 健壮性判断，如果then的传参不是函数就直接给一个返回参数值的函数
+    // 判断参数是否为函数，如果不是就直接给一个返回value的函数
     onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
     onRejected =
       typeof onRejected === 'function'
@@ -273,49 +108,53 @@ class MyPromise {
           };
     let promise;
     promise = new MyPromise((resolve, reject) => {
+      // executor的状态为fulfilled
       if (this.status === FULFILLED) {
-        resolvePromise(promise, onFulfilled(this.value), resolve, reject);
+        setTimeout(() => {
+          resolvePromise(promise, onFulfilled(this.value), resolve, reject);
+        }, 0);
       }
 
+      // executor的状态为rejected
       if (this.status === REJECTED) {
-        resolvePromise(promise, onRejected(this.reason), resolve, reject);
+        setTimeout(() => {
+          resolvePromise(promise, onRejected(this.err), resolve, reject);
+        }, 0);
       }
 
-      // 如果还是pending，就将then缓存到thenArr
+      // 此处用来解决异步问题，如果依然是pending的话，可以先将then里面的回调函数放入队列，等待resolve或者reject再统一执行
       if (this.status === PENDING) {
         this.thenArr.push(() => {
-          resolvePromise(promise, onFulfilled(this.value), resolve, reject);
+          setTimeout(() => {
+            resolvePromise(promise, onFulfilled(this.value), resolve, reject);
+          }, 0);
         });
         this.catchArr.push(() => {
-          resolvePromise(promise, onRejected(this.reason), resolve, reject);
+          setTimeout(() => {
+            resolvePromise(promise, onRejected(this.err), resolve, reject);
+          }, 0);
         });
       }
-    }, `${this.flag} then return`);
+    });
     return promise;
+  }
+
+  catch(onRejected) {
+    return this.then(null, onRejected);
   }
 }
 
 const promiseA = new MyPromise((resolve, reject) => {
   setTimeout(() => {
-    resolve('data from promiseA');
+    resolve(1);
   }, 1000);
-}, 'promiseA');
-
-// const promiseB = new MyPromise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve('data from promiseB');
-//   }, 1000);
-// }, 'promiseB');
-
-// promiseA
-//   .then(res => {
-//     console.log(`handle ${res}`);
-//     return promiseB;
-//   })
-//   .then(res => {
-//     console.log(res);
-//   });
-
-MyPromise.resolve(11).then(res => {
-  console.log(res);
 });
+
+promiseA.then(
+  res => {
+    document.write(res);
+  },
+  err => {
+    console.log(err);
+  }
+);
